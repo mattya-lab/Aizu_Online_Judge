8ae5646ee9fafaa5ba72b71cf70d138490aa55f5
mattya-lab <61013032+mattya-lab@users.noreply.github.com>
2020-06-18 09:49:26 +0900

    Delete 0005.cpp

--- Volume0/0005.cpp
+++
@@ -1,7 +0,0 @@
-int gcd(int x, int y) { return (x % y == 0) ? y : gcd(y, x % y); }
-
-int main(void) {
-	int a, b; cin >> a >> b;
-	cout << gcd(a, b) << " " << (a * b) / gcd(a, b) << endl;
-	return 0;
-}
