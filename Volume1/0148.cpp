346f2e224e2e01c982c9311b98d9be3c79b1859b
mattya-lab <61013032+mattya-lab@users.noreply.github.com>
2020-09-14 14:45:41 +0900

    Add files via upload

---
+++ Volume1/0148.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < n; i++)
+using p = pair<int, int>;
+typedef long long ll;
+#define INF (999)
+
+int main(void) 
+{
+	rep(z, 4) {
+		int a; cin >> a;
+		int n = 39;
+
+		a = a % 39;
+		if (a == 0) { a = 39; }
+		char sa[4];  
+		if (a < 10) { sprintf(sa,"3c0%d", a); }
+		else { sprintf(sa, "3c%d", a); }
+		cout << sa << endl;
+	}
+	return 0;
+}
